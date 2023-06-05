9095e30fda470704d7dddbd32c16edb980ad47ae
Denys-Kondratenko <dk132652@gmail.com>
2023-06-05 22:19:30 +0300

    встановив Redux Toolkit, створив файл store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
+import { store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 const theme = {
   colors: {
@@ -19,7 +21,9 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
